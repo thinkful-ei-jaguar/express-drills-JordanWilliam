831139fdb6d753c5f8fd24605f9a28856dd7529c
Jordan Castillo <jordanxcallaway@gmail.com>
2020-01-21 14:23:23 -0800

    Drill 3 done

--- app.js
+++ app.js
@@ -77,3 +77,41 @@ app.get('/cipher', (req, res) => {
 });
 
 //DRILL #3
+app.get('/lotto', (req, res) => {
+  const { numbers } = req.query;
+
+  //validation: the numbers array must exist, consist of 6 numbers and numbers must be between 1 and 20 
+  if(!numbers){
+    return res.status(400).send('numbers array is required');
+  } 
+  const numsInArray = numbers.map(num => +num).filter(num => num >= 1 && num <= 20);
+  console.log('numsInArray:', numsInArray);
+  if (numsInArray.length !== 6){
+    return res.status(400).send('must be 6 integers in the numbers array and all numbers must be between 1 and 20');
+  }
+
+  //randomly generate 6 integers between 1 and 20 as the winning numbers to compare the query to
+  var winningNumbers = [...Array(6)].map(() => Math.floor(Math.random() * 20));
+  console.log('winning numbers:', winningNumbers);
+
+
+  let responsePhrase;
+
+  //compare the integers in the query to the intgers randomly generated 
+  const comparingNumbers = winningNumbers.filter(num => numsInArray.includes(num));
+  //if less than 4 numbers match, respond with 'Sorry, you lose'
+  if(comparingNumbers.length < 4 ){
+    responsePhrase = 'Sorry, you lose';
+  }
+  //if at least 4 numbers match, repond with 'Congrats, you win'
+  if (comparingNumbers.length >= 4){
+    responsePhrase = 'Congrats, you win!';
+  }
+  //if all 6 numbers match, respond with 'Wow, you win a million dollars!'
+  if(comparingNumbers.length === 6){
+    responsePhrase = 'Wow, you win a million dollars!';
+  }
+
+
+  res.send(responsePhrase);
+});
